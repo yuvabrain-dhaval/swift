c30daa78a8f45a92d76ad55e598033ac88cc6a9d
Dhaval Patoliya <yuvabrain.dp@gmail.com>
2019-03-09 19:42:26 +0530

    FirstDemoProject

---
+++ DynamicCollectionDemo/DynamicCollectionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DynamicCollectionDemo
+//
+//  Created by Pulkit on 09/03/19.
+//  Copyright © 2019 Yuvabrain. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
